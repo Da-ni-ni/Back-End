a5660fe7e2e85e5b04388976f7a76cdd99616f9a
SOoOBn <sb2747@naver.com>
2025-05-19 15:33:56 +0900

    WebConfig 추가

---
+++ src/main/java/da_ni_ni/backend/user/jwt/WebConfig.java
@@ -0,0 +1,22 @@
+package da_ni_ni.backend.user.jwt;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // 실사용시 특정 도메인만 허용하는 게 더 안전
+                        .allowedMethods("*");
+            }
+        };
+    }
+}
+
